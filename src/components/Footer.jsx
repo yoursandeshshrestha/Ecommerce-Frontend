f01f9a1bde32dc802f11676be2b49e248ee222a3
Sandesh Shrestha <yoursandeshgeneral@gmail.com>
2024-07-22 23:46:21 +0530

    footer added *just try

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="Footer-Container">
+      <div className="Footer-section-one">
+        <h3>SHOP</h3>
+        <div className="section-one-links">
+          <p>Ladies</p>
+          <p>Men</p>
+          <p>Baby</p>
+          <p>Kids</p>
+        </div>
+      </div>
+      <div className="Footer-section-two">
+        <h3>CORPORATE INFO</h3>
+        <div className="section-two-links">
+          <p>Career at H&M</p>
+          <p>About H&M group</p>
+          <p>Sustainability H&M Group</p>
+          <p>Press</p>
+          <p>Investor relations</p>
+          <p>Corporate governance</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
